36ac93da4952fd66c9fde1d5b16691eae9b0b398
Pholenk <pholenkadi17@gmail.com>
2017-07-18 20:54:08 +0700

    modified sidebar baak

--- application/views/sidebar/baak.php
+++ application/views/sidebar/baak.php
@@ -29,13 +29,14 @@
           <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="/users"><i class="fa fa-users"></i> Pengguna</a></li>
-          <li><a href="/mahasiswa"><i class="fa fa-graduation-cap"></i> Mahasiswa</a></li>
-          <li><a href="/orangtua"><i class="fa fa-envira"></i> Orang Tua Mahasiswa</a></li>
-          <li><a href="/dosen"><i class="fa fa-wrench"></i> Dosen</a></li>
-          <li><a href="/jurusan"><i class="fa fa-star"></i> Jurusan</a></li>
-          <li><a href="/matakuliah"><i class="fa fa-money"></i> Mata Kuliah</a></li>
-          <li><a href="/uangkuliah"><i class="fa fa-industry"></i> Uang Kuliah</a></li>
+          <li><a href="/pengguna"><i class="fa fa-dot-circle-o"></i> Pengguna</a></li>
+          <li><a href="/mahasiswa"><i class="fa fa-dot-circle-o"></i> Mahasiswa</a></li>
+          <li><a href="/orangtua"><i class="fa fa-dot-circle-o"></i> Orang Tua Mahasiswa</a></li>
+          <li><a href="/jurusan"><i class="fa fa-dot-circle-o"></i> Jurusan</a></li>
+          <li><a href="/dosen"><i class="fa fa-dot-circle-o"></i> Dosen</a></li>
+          <li><a href="/matakuliah"><i class="fa fa-dot-circle-o"></i> Mata Kuliah</a></li>
+          <li><a href="/ajar"><i class="fa fa-dot-circle-o"></i> Mengajar</a></li>
+          <li><a href="/uangkuliah"><i class="fa fa-dot-circle-o"></i> Uang Kuliah</a></li>
           <li>
             <a href="#"><i class="fa fa-truck"></i> Supplier
               <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
