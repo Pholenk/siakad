fdf4f31659f70099d1f70bffd75634f591614ecc
Pholenk <pholenkadi17@gmail.com>
2017-07-19 14:59:32 +0700

    add bootstrap select script

--- application/views/foot.php
+++ application/views/foot.php
@@ -12,22 +12,12 @@ $.widget.bridge('uibutton', $.ui.button);
 <script src="/vendor/almasaeed2010/adminlte/bootstrap/js/bootstrap.min.js"></script>
 <!-- Custom js -->
 <script src="/assets/js/custom.js"></script>
-<!-- dataTables js -->
-<script src="/vendor/almasaeed2010/adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="/vendor/almasaeed2010/adminlte/plugins/datatables/dataTables.bootstrap.min.js"></script>
-<script src="/vendor/almasaeed2010/adminlte/plugins/datatables/extensions/Select/js/dataTables.select.min.js"></script>
+<!-- bootstrap-select js -->
+<script src="/assets/js/bootstrap-select/dist/js/bootstrap-select.js"></script>
 <!-- iCheck js -->
 <script src="/vendor/almasaeed2010/adminlte/plugins/iCheck/icheck.min.js"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="/vendor/almasaeed2010/adminlte/plugins/morris/morris.min.js"></script>
 <!-- Sparkline -->
 <script src="/vendor/almasaeed2010/adminlte/plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="/vendor/almasaeed2010/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="/vendor/almasaeed2010/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="/vendor/almasaeed2010/adminlte/plugins/knob/jquery.knob.js"></script>
 <!-- daterangepicker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
 <script src="/vendor/almasaeed2010/adminlte/plugins/daterangepicker/daterangepicker.js"></script>
