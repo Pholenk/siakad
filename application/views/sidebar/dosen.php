a3e031986359724f58ba5a3329d037fb93288e1e
Pholenk <pholenkadi17@gmail.com>
2017-07-26 19:03:38 +0700

    modified sidebar dosen

---
+++ application/views/sidebar/dosen.php
@@ -0,0 +1,42 @@
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- Sidebar user panel -->
+    <div class="user-panel" style="padding-top:20px;padding-bottom:20px;">
+      <div class="pull-left image">
+        <img src="/vendor/almasaeed2010/adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        <?php //echo '/assets/image/'.$this->session->id.'jpg'?>
+      </div>
+      <div class="pull-left info">
+        <p style='padding-bottom:2px;'>
+          <?php echo $this->session->name ?>
+        </p>
+        <i class="fa fa-circle text-success"></i>
+        <?php echo $this->session->job ?>
+      </div>
+    </div>
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu">
+      <li class="header">Main Sidebar</li>
+      <li class="treeview">
+        <a href="/"><i class="fa fa-dashboard"></i><span>Dashboard</span></a>
+      </li>
+      <li class="header">Penilaian</li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-money"></i>
+          <span>Penilaian</span>
+          <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+        </a>
+        <ul class="treeview-menu">
+          <li class="treview"><a href="/nilai/"><i class="fa fa-dot-circle-o"></i> Show Nilai</a></li>
+          <li class="treview"><a href="/nilai/add"><i class="fa fa-dot-circle-o"></i> Add Nilai</a></li>
+          <li class="treview"><a href=""><i class="fa fa-dot-circle-o"></i> Edit Nilai</a></li>
+        </ul>
+      </li>
+      <li class="header">Cetak</li>
+    </ul>
+    <!-- /.sidebar menu -->
+  </section>
+  <!-- /.sidebar -->
+</aside>
