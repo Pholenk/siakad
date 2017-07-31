4fa8a8d5c2610f90ef53126844e9df5a57c8b77a
Pholenk <pholenkadi17@gmail.com>
2017-07-31 08:44:00 +0700

    create print form kartu ujian

---
+++ application/views/kartu-ujian.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SIAKAD</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!--link rel="shortcut icon" type="image/x-icon" href="../../assets/image/logo_pnc_hea_icon.ico"-->
+  <!-- custom.css>
+  <link rel="stylesheet" href="../../assets/css/custom.css" -->
+  <!-- bootstrap.css -->
+  <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/bootstrap/css/bootstrap.min.css">
+  <style>
+  table>tr>th,
+  table>tr>td {
+  	
+  }
+  </style>
+</head>
+<body>
+	<div class="row" style="margin-left:0;">
+		<div class="col-xs-4" style="border:2px solid black;padding:2px;margin-bottom:0px;">
+			<p style="margin:0 3px 0 10px;font-size:12px;letter-spacing:0.2px;text-align:justify">KEMENTERIAN RISET TEKNOLOGI dan PENDIDIKAN TINGGI</p>
+			<h4 style="margin:0 3px 0 10px;letter-spacing:3.5px;text-align:justify">POLITEKNIK NEGERI CILACAP</h4>
+			<p style="margin:0 3px 0 10px;font-size:12px;letter-spacing:0.4px;text-align:justify">Jalan Dr. Soetomo No.1, Sidakaya, Cilacap, Jawa Tengah</p>
+			<p style="margin:0 3px 0 10px;font-size:12px;letter-spacing:1.3px;text-align:justify">Telepon (0282) 533329 Faksimile (0282) 537992</p>
+			<p style="margin:0 3px 0 10px;font-size:11px;text-align:justify">http://www.politeknikcilacap.ac.id, poltec@politeknikcilacap.ac.id</p>
+			<p style="border-top: 2px solid black;"></p>
+			<p style="margin:5px 3px 15px 10px;text-align:center;padding:2px;">KARTU PESERTA UJIAN TENGAH SEMESTER GANJIL TAHUN AKADEMIK 2017 / 2018</p>
+			<div class="col-xs-12" style="text-align:justify;">
+				<p>NAMA : Bramanditya Adi Prabowo</p>
+			</div>
+			<div class="col-xs-12" style="text-align:justify;">
+				<p>NIM : 14.06.02.0049</p>
+			</div>
+			<div class="col-xs-12" style="text-align:justify;">
+				<p>JURUSAN : TEKNIK INFORMATIKA</p>
+			</div>
+			<div class="col-xs-12" style="text-align:justify;">
+				<p>SEMESTER / KELAS : VI / C</p>
+			</div>
+			<div class="col-xs-12" style="text-align:justify;">
+				<p>DOSEN WALI : ANTONIUS AGUNG HARTONO, S.T.</p>
+			</div>
+		<p style="margin-left:45%;">Cilacap, 31 September 2017</p>
+		<p style="margin-left:45%;">Ketua Panitia</p>
+		<p style="margin-left:45%;margin-top:50px;margin-bottom:0px;text-decoration:underline;">Muhammad Yusuf, S. ST., MT</p>
+		<p style="margin-left:45%;margin-top:0px;font-weight:bold;">NPAK : 09.08.8010</p>
+		</div>
+		<div class="col-xs-7" style="border:2px solid black;padding:2px;margin-bottom:0px;">
+			<h4 style="margin:10px 3px 15px 10px;text-align:center;padding:2px;">Jadwal Ujian Tengah Semester</h4>
+			<table class="table" style="margin-left:2%;margin-right:2%;margin-bottom:13%;border:1px solid black;">
+				<thead>
+					<tr>
+						<th style="border: 1px solid black;text-align:center;">Hari / Tanggal</th>
+						<th style="border: 1px solid black;text-align:center;">Jam</th>
+						<th style="border: 1px solid black;text-align:center;">Ruang</th>
+						<th style="border: 1px solid black;text-align:center;height:30px;">Mata Kuliah</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
+						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
