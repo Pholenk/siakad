50391dd8b7949cbf8aa69026874cd87a272e8953
Pholenk <pholenkadi17@gmail.com>
2017-08-07 07:16:39 +0700

    take some adjustment

--- application/views/head.php
+++ application/views/head.php
@@ -4,7 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>System Management Farm</title>
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="shortcut icon"  type"image/x-icon" href="/assets/image/logo_pnc.ico">
+  <title>PNC SIAKAD</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -13,26 +15,17 @@
   <link rel="stylesheet" href="/assets/css/custom.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="/assets/css/font-awesome-4.7.0/css/font-awesome.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Ionicons >
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"-->
   <!-- Theme style -->
   <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/dist/css/skins/_all-skins.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/plugins/iCheck/flat/green.css">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/plugins/morris/morris.css">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
   <!-- Date Picker -->
   <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/plugins/datepicker/datepicker3.css">
   <!-- Daterange picker -->
   <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/plugins/daterangepicker/daterangepicker.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/plugins/datatables/dataTables.bootstrap.css">
-  <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/plugins/datatables/extensions/Select/css/select.semanticui.min.css">
   <!-- bootstrap wysihtml5 - text editor -->
   <link rel="stylesheet" href="/vendor/almasaeed2010/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
