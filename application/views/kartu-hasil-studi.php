c3b7087e8ea2ac0eb2ee7c2c161343192fa478c2
Pholenk <pholenkadi17@gmail.com>
2017-08-07 07:19:33 +0700

    create khs template

---
+++ application/views/kartu-hasil-studi.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SIAKAD</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!--link rel="shortcut icon" type="image/x-icon" href="../../assets/image/logo_pnc_hea_icon.ico"-->
+  <!-- custom.css>
+  <link rel="stylesheet" href="../../assets/css/custom.css" -->
+  <!-- bootstrap.css -->
+  <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/bootstrap/css/bootstrap.min.css">
+</head>
+<body style="margin:0;">
+	<div class="row" style="margin:0;padding:2%;">
+		<div class="col-xs-12" style="margin:0 0 2% 0;border-bottom: 4px solid black;">
+			<div class="col-xs-2">
+				<img src="../../assets/image/logo-pnc.png" style="width:160px;">
+			</div>
+			<div class="col-xs-9" style="margin-bottom:2%;">
+				<h3 style="margin-top:0px;word-space:50px;letter-spacing:1.5px;text-align:justify">KEMENTERIAN RISET TEKNOLOGI dan PENDIDIKAN TINGGI</h3>
+				<h2 style="word-space:50px;letter-spacing:12.5px;text-align:justify">POLITEKNIK NEGERI CILACAP</h2>
+				<h3 style="margin:4px 0 0 0;font-size:22px;letter-spacing:2.7px;text-align:justify">Jalan Dr. Soetomo No.1, Sidakaya, Cilacap, Jawa Tengah</h3>
+				<h3 style="margin:0;font-size:22px;letter-spacing:4.4px;text-align:justify">Telepon (0282) 533329 Faksimile (0282) 537992</h3>
+				<h3 style="margin:0;font-size:22px;letter-spacing:0.8px;text-align:justify">http://www.politeknikcilacap.ac.id, poltec@politeknikcilacap.ac.id</h3>
+			</div>
+		</div>
+		<div class="col-xs-12" style="border-bottom: 2px solid black;border-top: 2px solid black;">
+			<h3 style="text-align:center;vertical-align:middle;"> KARTU HASIL STUDI </h3>
+		</div>
+		<div class="row" style="margin-top:2%;">
+			<div class="col-xs-12" style="">
+				<div class="col-xs-5" style="text-align:justify;">
+					<h4>NAMA : Bramanditya Adi Prabowo</h4>
+				</div>
+				<div class="col-xs-5 pull-right" style="text-align:justify;">
+					<h4>JURUSAN : TEKNIK INFORMATIKA</h4>
+				</div>
+			</div>
+			<div class="col-xs-12" style="">
+				<div class="col-xs-5" style="text-align:justify;">
+					<h4>NIM : 14.06.02.0049</h4>
+				</div>
+				<div class="col-xs-5 pull-right" style="text-align:justify;">
+					<h4>JENJANG : DIPLOMA III POLITEKNIK</h4>
+				</div>
+			</div>
+			<div class="col-xs-12" style="">
+				<div class="col-xs-5" style="text-align:justify;">
+					<h4>SEMESTER / TAHUN AJARAN : VI / 2017-2018</h4>
+				</div>
+				<div class="col-xs-5 pull-right" style="text-align:justify;">
+					<h4>KELAS : C</h4>
+				</div>
+			</div>
+		</div>
+		<div class="row" style="padding:2%;">
+			<table class="col-xs-12">
+				<thead>
+					<tr>
+						<th style="border: 1px solid black;padding:10px;text-align:center;height:30px;width:40px;">No.</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;width:200px;">KODE MK</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;">MATA KULIAH</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;width:100px;">SKS</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;width:100px;word-wrap:break-word">NILAI HURUF</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;width:100px;word-wrap:break-word">NILAI MUTU</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;width:250px;">KET</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">1</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">MU04</td>
+						<td style="border: 1px solid black;padding:10px;text-align:left;">PENDIDIKAN AGAMA</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">AB</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">7</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">MU05</td>
+						<td style="border: 1px solid black;padding:10px;text-align:left;">BAHASA INDONESIA</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">AB</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">7</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">3</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">TI238T</td>
+						<td style="border: 1px solid black;padding:10px;text-align:left;">KESEHATAN KESELAMATAN KERJA (K3)</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">B</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">6</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">4</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">TI239T</td>
+						<td style="border: 1px solid black;padding:10px;text-align:left;">TEKNIK SUPERVISI</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">AB</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">7</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
+					</tr>
+					<tr>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">5</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">MU1006P</td>
+						<td style="border: 1px solid black;padding:10px;text-align:left;">MAGANG INDUSTRI</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">10</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">A</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;">40</td>
+						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
+					</tr>
+					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="3" style="padding:10px;text-align:right;">JUMLAH : </td>
+						<td style="padding:10px;text-align:center;">18</td>
+						<td style="padding:10px;text-align:right;"></td>
+						<td style="padding:10px;text-align:center;">67</td>
+						<td style="padding:10px;text-align:right;"></td>
+					</tr>
+					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="2" style="padding:10px;text-align:left;">INDEKS PRESTASI</td>
+						<td colspan="5" style="padding:10px;text-align:left;"> : 3.72 </td>
+					</tr>
+					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="2" style="padding:10px;text-align:left;">INDEKS PRESTASI KUMULATIF</td>
+						<td colspan="5" style="padding:10px;text-align:left;"> : 3.38 </td>
+					</tr>
+					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="2" style="padding:10px;text-align:left;">STATUS KELULUSAN</td>
+						<td colspan="5" style="padding:10px;text-align:left;"> : LULUS </td>
+					</tr>
+					<tr style="border-bottom: 1px solid black;border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="7" style="padding:10px;text-align:left;"></td>
+					</tr>
+					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="1" style="padding:10px;text-align:left;"></td>
+						<td colspan="6" style="padding:10px;text-align:left;">KETIDAKHADIRAN KARENA</td>
+					</tr>
+					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="3" style="padding:10px;text-align:center;">ALPA</td>
+						<td colspan="4" style="padding:10px;text-align:left;"> : 0 </td>
+					</tr>
+					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="3" style="padding:10px;text-align:center;">IJIN</td>
+						<td colspan="4" style="padding:10px;text-align:left;"> : 0 </td>
+					</tr>
+					<tr style="border-bottom: 1px solid black;border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="3" style="padding:10px;text-align:center;">SAKIT</td>
+						<td colspan="4" style="padding:10px;text-align:left;"> : 0 </td>
+					</tr>
+
+				</tbody>
+			</table>
+			
+			
+			
+			
+		</div>
+	</div>
+</body>
+</html>
