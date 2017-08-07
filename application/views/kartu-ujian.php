54f161faa892e82d57540412524311db76b9bb30
Pholenk <pholenkadi17@gmail.com>
2017-08-07 07:17:09 +0700

    take some adjustment

--- application/views/kartu-ujian.php
+++ application/views/kartu-ujian.php
@@ -12,105 +12,68 @@
   <link rel="stylesheet" href="../../assets/css/custom.css" -->
   <!-- bootstrap.css -->
   <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/bootstrap/css/bootstrap.min.css">
-  <style>
-  table>tr>th,
-  table>tr>td {
-  	
-  }
-  </style>
 </head>
-<body>
+<body style="">
 	<div class="row" style="margin-left:0;">
-		<div class="col-xs-4" style="border:2px solid black;padding:2px;margin-bottom:0px;">
+		<div class="col-xs-4" style="border:2px solid black;padding:2px;margin-top:50px;">
 			<p style="margin:0 3px 0 10px;font-size:12px;letter-spacing:0.2px;text-align:justify">KEMENTERIAN RISET TEKNOLOGI dan PENDIDIKAN TINGGI</p>
 			<h4 style="margin:0 3px 0 10px;letter-spacing:3.5px;text-align:justify">POLITEKNIK NEGERI CILACAP</h4>
 			<p style="margin:0 3px 0 10px;font-size:12px;letter-spacing:0.4px;text-align:justify">Jalan Dr. Soetomo No.1, Sidakaya, Cilacap, Jawa Tengah</p>
 			<p style="margin:0 3px 0 10px;font-size:12px;letter-spacing:1.3px;text-align:justify">Telepon (0282) 533329 Faksimile (0282) 537992</p>
 			<p style="margin:0 3px 0 10px;font-size:11px;text-align:justify">http://www.politeknikcilacap.ac.id, poltec@politeknikcilacap.ac.id</p>
 			<p style="border-top: 2px solid black;"></p>
-			<p style="margin:5px 3px 15px 10px;text-align:center;padding:2px;">KARTU PESERTA UJIAN TENGAH SEMESTER GANJIL TAHUN AKADEMIK 2017 / 2018</p>
+			<p style="margin:5px 3px 15px 10px;text-align:center;padding:2px;word-spacing:10px;letter-spacing:3px;">KARTU PESERTA UJIAN <?php echo str_replace('_', ' ', $ujian)." TAHUN AKADEMIK ".date('Y', now())." / ".date('Y', strtotime("+1 Year"))?></p>
+			<?php foreach ($mahasiswas as $mahasiswa)
+			{
+			?>
 			<div class="col-xs-12" style="text-align:justify;">
-				<p>NAMA : Bramanditya Adi Prabowo</p>
+				<p>NAMA : <?php echo $mahasiswa->nama; ?></p>
 			</div>
 			<div class="col-xs-12" style="text-align:justify;">
-				<p>NIM : 14.06.02.0049</p>
+				<p>NIM : <?php echo $mahasiswa->nim; ?></p>
 			</div>
 			<div class="col-xs-12" style="text-align:justify;">
-				<p>JURUSAN : TEKNIK INFORMATIKA</p>
+				<p>JURUSAN : <?php echo $mahasiswa->jurusan; ?></p>
 			</div>
 			<div class="col-xs-12" style="text-align:justify;">
-				<p>SEMESTER / KELAS : VI / C</p>
+				<p>SEMESTER / KELAS : <?php echo $mahasiswa->semester.' / '.$mahasiswa->kelas; ?></p>
 			</div>
-			<div class="col-xs-12" style="text-align:justify;">
-				<p>DOSEN WALI : ANTONIUS AGUNG HARTONO, S.T.</p>
-			</div>
-		<p style="margin-left:45%;">Cilacap, 31 September 2017</p>
+			<?php } ?>
+		<p style="margin-left:45%;">Cilacap, <?php echo date('d M Y', now()); ?></p>
 		<p style="margin-left:45%;">Ketua Panitia</p>
-		<p style="margin-left:45%;margin-top:50px;margin-bottom:0px;text-decoration:underline;">Muhammad Yusuf, S. ST., MT</p>
-		<p style="margin-left:45%;margin-top:0px;font-weight:bold;">NPAK : 09.08.8010</p>
+		<p style="margin-left:45%;margin-top:50px;margin-bottom:0px;text-decoration:underline;"><?php echo $ketua_panitia ?></p>
+		<p style="margin-left:45%;margin-top:0px;font-weight:bold;">NPAK : <?php echo $npak; ?></p>
 		</div>
 		<div class="col-xs-7" style="border:2px solid black;padding:2px;margin-bottom:0px;">
 			<h4 style="margin:10px 3px 15px 10px;text-align:center;padding:2px;">Jadwal Ujian Tengah Semester</h4>
 			<table class="table" style="margin-left:2%;margin-right:2%;margin-bottom:13%;border:1px solid black;">
 				<thead>
 					<tr>
-						<th style="border: 1px solid black;text-align:center;">Hari / Tanggal</th>
+						<th style="border: 1px solid black;text-align:center;">Tanggal</th>
 						<th style="border: 1px solid black;text-align:center;">Jam</th>
 						<th style="border: 1px solid black;text-align:center;">Ruang</th>
 						<th style="border: 1px solid black;text-align:center;height:30px;">Mata Kuliah</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td rowspan="2" style="border: 1px solid black;vertical-align:middle;text-align:center;">Senin / 25 Juli 2018</td>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td rowspan="2" style="border: 1px solid black;text-align:center;">E1.5</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;text-align:center;">14.00 - 15.30</td>
-						<td style="border: 1px solid black;text-align:center;height:30px;">Manajemen Produksi</td>
-					</tr>
+					<?php for ($i=1; $i < 8 ; $i++) { 
+						echo "<tr>
+							<td rowspan='2' style='border: 1px solid black;vertical-align:middle;text-align:center;'>".$tgl_ujian[''.$i]."</td>";
+						 for ($j=1; $j < 2 ; $j++) { 
+						 	echo "
+								<td style='border: 1px solid black;text-align:center;'>".$jam_ujian[''.$i][''.$j]."</td>
+								<td rowspan='2' style='border: 1px solid black;text-align:center;'>".$ruangan."</td>
+								<td style='border: 1px solid black;text-align:center;height:30px;'>".$jadwal_ujian[''.$i][''.$j]."</td>
+							</tr>";
+						 } 
+						 for ($j=2; $j < 3 ; $j++) { 
+						 	echo "
+							<tr>
+								<td style='border: 1px solid black;text-align:center;'>".$jam_ujian[''.$i][''.$j]."</td>
+								<td style='border: 1px solid black;text-align:center;height:30px;'>".$jadwal_ujian[''.$i][''.$j]."</td>
+							</tr>";
+						 } 
+					 } ?>
 				</tbody>
 			</table>
 		</div>
