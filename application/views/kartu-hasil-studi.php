3411732e06e37522c25e4552f5db3ac265d19986
Pholenk <pholenkadi17@gmail.com>
2017-08-13 17:36:18 +0700

    modified kartu hasil studi views

--- application/views/kartu-hasil-studi.php
+++ application/views/kartu-hasil-studi.php
@@ -20,146 +20,97 @@
 				<img src="../../assets/image/logo-pnc.png" style="width:160px;">
 			</div>
 			<div class="col-xs-9" style="margin-bottom:2%;">
-				<h3 style="margin-top:0px;word-space:50px;letter-spacing:1.5px;text-align:justify">KEMENTERIAN RISET TEKNOLOGI dan PENDIDIKAN TINGGI</h3>
-				<h2 style="word-space:50px;letter-spacing:12.5px;text-align:justify">POLITEKNIK NEGERI CILACAP</h2>
-				<h3 style="margin:4px 0 0 0;font-size:22px;letter-spacing:2.7px;text-align:justify">Jalan Dr. Soetomo No.1, Sidakaya, Cilacap, Jawa Tengah</h3>
-				<h3 style="margin:0;font-size:22px;letter-spacing:4.4px;text-align:justify">Telepon (0282) 533329 Faksimile (0282) 537992</h3>
-				<h3 style="margin:0;font-size:22px;letter-spacing:0.8px;text-align:justify">http://www.politeknikcilacap.ac.id, poltec@politeknikcilacap.ac.id</h3>
+				<h3 style="margin-top:0px;margin-bottom:0px;font-size:20px;text-align:justify">KEMENTERIAN RISET TEKNOLOGI dan PENDIDIKAN TINGGI</h3>
+				<h2 style="margin-top:0px;margin-bottom:0px;word-space:5px;letter-spacing:5px;text-align:justify">POLITEKNIK NEGERI CILACAP</h2>
+				<h3 style="margin:4px 0 0 0;font-size:19px;letter-spacing:1px;text-align:justify">Jalan Dr. Soetomo No.1, Sidakaya, Cilacap, Jawa Tengah</h3>
+				<h3 style="margin:0;font-size:19px;letter-spacing:0.8px;text-align:justify">Telepon (0282) 533329 Faksimile (0282) 537992</h3>
+				<h3 style="margin:0;font-size:18px;text-align:justify">http://www.politeknikcilacap.ac.id, poltec@politeknikcilacap.ac.id</h3>
 			</div>
 		</div>
 		<div class="col-xs-12" style="border-bottom: 2px solid black;border-top: 2px solid black;">
 			<h3 style="text-align:center;vertical-align:middle;"> KARTU HASIL STUDI </h3>
 		</div>
 		<div class="row" style="margin-top:2%;">
+			<?php foreach ($self_data as $mahasiswa) { ?>
 			<div class="col-xs-12" style="">
 				<div class="col-xs-5" style="text-align:justify;">
-					<h4>NAMA : Bramanditya Adi Prabowo</h4>
+					<h5>NAMA : <?php echo $mahasiswa->nama ;?></h5>
 				</div>
 				<div class="col-xs-5 pull-right" style="text-align:justify;">
-					<h4>JURUSAN : TEKNIK INFORMATIKA</h4>
+					<h5>JURUSAN : <?php echo $mahasiswa->jurusan ;?></h5>
 				</div>
 			</div>
 			<div class="col-xs-12" style="">
 				<div class="col-xs-5" style="text-align:justify;">
-					<h4>NIM : 14.06.02.0049</h4>
+					<h5>NIM : <?php echo $mahasiswa->nim ;?></h5>
 				</div>
 				<div class="col-xs-5 pull-right" style="text-align:justify;">
-					<h4>JENJANG : DIPLOMA III POLITEKNIK</h4>
+					<h5>JENJANG : DIPLOMA III POLITEKNIK</h5>
 				</div>
 			</div>
 			<div class="col-xs-12" style="">
 				<div class="col-xs-5" style="text-align:justify;">
-					<h4>SEMESTER / TAHUN AJARAN : VI / 2017-2018</h4>
+					<h5>SEMESTER / TAHUN AJARAN : <?php echo $semester; ?> / <?php echo date('Y',now())." - ".date('Y', strtotime("+1 Year")); ?></h5>
 				</div>
 				<div class="col-xs-5 pull-right" style="text-align:justify;">
-					<h4>KELAS : C</h4>
+					<h5>KELAS : <?php echo $mahasiswa->kelas ;?></h5>
 				</div>
 			</div>
+			<?php } ?>
 		</div>
 		<div class="row" style="padding:2%;">
 			<table class="col-xs-12">
 				<thead>
 					<tr>
-						<th style="border: 1px solid black;padding:10px;text-align:center;height:30px;width:40px;">No.</th>
-						<th style="border: 1px solid black;padding:10px;text-align:center;width:200px;">KODE MK</th>
-						<th style="border: 1px solid black;padding:10px;text-align:center;">MATA KULIAH</th>
-						<th style="border: 1px solid black;padding:10px;text-align:center;width:100px;">SKS</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;height:20px;width:40px;">No.</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;width:100px;">KODE MK</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;width:250px;">MATA KULIAH</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;width:50px;">SKS</th>
 						<th style="border: 1px solid black;padding:10px;text-align:center;width:100px;word-wrap:break-word">NILAI HURUF</th>
 						<th style="border: 1px solid black;padding:10px;text-align:center;width:100px;word-wrap:break-word">NILAI MUTU</th>
-						<th style="border: 1px solid black;padding:10px;text-align:center;width:250px;">KET</th>
+						<th style="border: 1px solid black;padding:10px;text-align:center;">KET</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">1</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">MU04</td>
-						<td style="border: 1px solid black;padding:10px;text-align:left;">PENDIDIKAN AGAMA</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">AB</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">7</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">MU05</td>
-						<td style="border: 1px solid black;padding:10px;text-align:left;">BAHASA INDONESIA</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">AB</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">7</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">3</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">TI238T</td>
-						<td style="border: 1px solid black;padding:10px;text-align:left;">KESEHATAN KESELAMATAN KERJA (K3)</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">B</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">6</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">4</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">TI239T</td>
-						<td style="border: 1px solid black;padding:10px;text-align:left;">TEKNIK SUPERVISI</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">2</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">AB</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">7</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
-					</tr>
-					<tr>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">5</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">MU1006P</td>
-						<td style="border: 1px solid black;padding:10px;text-align:left;">MAGANG INDUSTRI</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">10</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">A</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;">40</td>
-						<td style="border: 1px solid black;padding:10px;text-align:center;"></td>
+					<?php $i=1;
+					foreach ($matakuliahs as $matakuliah) {
+						echo "
+						<tr>
+							<td style='border: 1px solid black;padding:5px;text-align:center;'>".$i."</td>
+							<td style='border: 1px solid black;padding:5px;text-align:center;'>".$matakuliah->id_matakuliah."</td>
+							<td style='border: 1px solid black;padding:5px;text-align:left;'>".$matakuliah->nama."</td>
+							<td style='border: 1px solid black;padding:5px;text-align:center;'>".$matakuliah->sks."</td>
+							<td style='border: 1px solid black;padding:5px;text-align:center;'>".$nilai[''.$i][0]."</td>
+							<td style='border: 1px solid black;padding:5px;text-align:center;'>".$nilai[''.$i][1]."</td>
+							<td style='border: 1px solid black;padding:5px;text-align:center;'></td>
+						</tr>";
+						$i++;
+					}
+					?>
+					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
+						<td colspan="3" style="padding:5px;text-align:right;">JUMLAH :</td>
+						<td style="padding:5px;text-align:center;"><?php echo $jumlah_sks; ?> </td>
+						<td style="padding:5px;text-align:center;"></td>
+						<td style="padding:5px;text-align:center;"><?php echo $jumlah_nilai_mutu; ?> </td>
+						<td style="padding:5px;text-align:center;"></td>
 					</tr>
 					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
-						<td colspan="3" style="padding:10px;text-align:right;">JUMLAH : </td>
-						<td style="padding:10px;text-align:center;">18</td>
-						<td style="padding:10px;text-align:right;"></td>
-						<td style="padding:10px;text-align:center;">67</td>
-						<td style="padding:10px;text-align:right;"></td>
+						<td colspan="2" style="padding:5px;text-align:left;">INDEKS PRESTASI</td>
+						<td colspan="5" style="padding:5px;text-align:left;"> : <?php echo $ips; ?> </td>
 					</tr>
 					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
-						<td colspan="2" style="padding:10px;text-align:left;">INDEKS PRESTASI</td>
-						<td colspan="5" style="padding:10px;text-align:left;"> : 3.72 </td>
+						<td colspan="2" style="padding:5px;text-align:left;">INDEKS PRESTASI KUMULATIF</td>
+						<td colspan="5" style="padding:5px;text-align:left;"> : <?php echo $ipk; ?> </td>
 					</tr>
 					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
-						<td colspan="2" style="padding:10px;text-align:left;">INDEKS PRESTASI KUMULATIF</td>
-						<td colspan="5" style="padding:10px;text-align:left;"> : 3.38 </td>
-					</tr>
-					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
-						<td colspan="2" style="padding:10px;text-align:left;">STATUS KELULUSAN</td>
-						<td colspan="5" style="padding:10px;text-align:left;"> : LULUS </td>
+						<td colspan="2" style="padding:5px;text-align:left;">STATUS KELULUSAN</td>
+						<td colspan="5" style="padding:5px;text-align:left;"> : <?php echo $kelulusan; ?> </td>
 					</tr>
 					<tr style="border-bottom: 1px solid black;border-left: 1px solid black;border-right: 1px solid black;">
 						<td colspan="7" style="padding:10px;text-align:left;"></td>
 					</tr>
-					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
-						<td colspan="1" style="padding:10px;text-align:left;"></td>
-						<td colspan="6" style="padding:10px;text-align:left;">KETIDAKHADIRAN KARENA</td>
-					</tr>
-					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
-						<td colspan="3" style="padding:10px;text-align:center;">ALPA</td>
-						<td colspan="4" style="padding:10px;text-align:left;"> : 0 </td>
-					</tr>
-					<tr style="border-left: 1px solid black;border-right: 1px solid black;">
-						<td colspan="3" style="padding:10px;text-align:center;">IJIN</td>
-						<td colspan="4" style="padding:10px;text-align:left;"> : 0 </td>
-					</tr>
-					<tr style="border-bottom: 1px solid black;border-left: 1px solid black;border-right: 1px solid black;">
-						<td colspan="3" style="padding:10px;text-align:center;">SAKIT</td>
-						<td colspan="4" style="padding:10px;text-align:left;"> : 0 </td>
-					</tr>
-
 				</tbody>
 			</table>
-			
-			
-			
-			
 		</div>
 	</div>
 </body>
